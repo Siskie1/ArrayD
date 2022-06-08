f51cb07000b79632d6d3d2cb0f7d95cac403d4a2
Dell <Siskie@gmail.com>
2022-06-08 12:17:43 +0200

    Question 5

---
+++ src/testpkg/CarPack.java
@@ -0,0 +1,20 @@
+package testpkg;
+
+public class CarPack {
+    String cModel;
+    String fType;
+    int maxSpeed;
+    double engSize;
+    int gearSize;
+
+    public void DisplayAttr()
+    {
+        System.out.println("Toyota" + " " + cModel + " " + "is the model of the car " );
+        System.out.println("The car has" + " " +gearSize + " " + "gear");
+        System.out.println("The car's maximum speed is " + maxSpeed);
+        System.out.println("The car uses" + " " + fType + " " + "for fuel");
+
+
+    }
+
+}
