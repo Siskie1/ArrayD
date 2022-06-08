9c2cfc5f3a47df637fab0d1e368a9916b27acac0
Dell <Siskie@gmail.com>
2022-06-08 12:34:49 +0200

    Question 5

--- src/testpkg/CarDetails.java
+++ src/testpkg/CarDetails.java
@@ -13,6 +13,5 @@ public class CarDetails {
 
         toyota.DisplayAttr();
 
-
     }
 }
--- src/testpkg/CarPack.java
+++ src/testpkg/CarPack.java
@@ -10,10 +10,9 @@ public class CarPack {
     public void DisplayAttr()
     {
         System.out.println("Toyota" + " " + cModel + " " + "is the model of the car " );
-        System.out.println("The car has" + " " +gearSize + " " + "gear");
+        System.out.println("The car has" + " " +gearSize + " " + "gears");
         System.out.println("The car's maximum speed is " + maxSpeed);
         System.out.println("The car uses" + " " + fType + " " + "for fuel");
-
 
     }
 
