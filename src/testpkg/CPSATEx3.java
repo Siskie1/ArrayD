19c6383bf562010dca0568e8dfc39b93f366b4cf
Dell <Siskie@gmail.com>
2022-06-14 16:20:27 +0200

    Exercise3 no4

---
+++ src/testpkg/CPSATEx3.java
@@ -0,0 +1,17 @@
+package testpkg;
+
+public class CPSATEx3 {
+    public static void main(String args[]) {
+        int passMark = 60;
+        int myMark = 59;
+
+        if (myMark >= passMark)
+        {
+            System.out.println(" You Passed CP-SAT exam");
+        }
+
+        else {
+            System.out.println("You Failed the CP-SAT exam");
+             }
+    }
+}
